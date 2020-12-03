427a8b0e27bdc0fde4dfc1cea80e4b6b55e52851
S-Maks <cool.dan47@mail.ru>
2020-12-04 00:15:46 +0300

    added the function of finding an account by username

--- src/main/java/com/computerAccessoriesStore/repository/UserRepository.java
+++ src/main/java/com/computerAccessoriesStore/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.computerAccessoriesStore.repository;
 
 import com.computerAccessoriesStore.models.User;
-import com.computerAccessoriesStore.transfer.UserDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @EnableJpaRepositories
 public interface UserRepository extends JpaRepository<User,Long>{
+    User getByUsername(String username);
 }
